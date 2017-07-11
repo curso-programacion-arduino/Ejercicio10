49f3962680754479637912c2a909d65711f2084c
JorgeManzanares <jorge.manzanares4@gmail.com>
2017-07-11 19:21:15 +0200

    Ahora si con el led

--- Ejercicio10-JorgeManzanares/Ejercicio10-JorgeManzanares.ino
+++ Ejercicio10-JorgeManzanares/Ejercicio10-JorgeManzanares.ino
@@ -1,20 +1,31 @@
 #define PIN_BOTON_A 2
 #define PIN_BOTON_B 3
-int resultado;
+#define PIN_LED_1 4
+#define PIN_LED_2 5 //PWM
+#define PIN_LED_3 6 //PWM
+#define PIN_LED_4 7
+int resultado,resultado_anterior,i;
 
 void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
-
+  pinMode(PIN_LED_1, OUTPUT);
+  pinMode(PIN_LED_2, OUTPUT);
+  pinMode(PIN_LED_3, OUTPUT);
+  pinMode(PIN_LED_4, OUTPUT);
 }
 
 void loop() {
-  int flanco_B=detectaFlancoAscendente(PIN_BOTON_B);
-  
-  if(flanco_B==1)
+  int flanco_B = detectaFlancoAscendente(PIN_BOTON_B);
+
+  if (flanco_B == 1)
   {
     Serial.println("RESULTADO");
-    resultado=random(0,5);
+    
+    resultado = random(4, 8);
+    digitalWrite(resultado_anterior,LOW);
+    digitalWrite(resultado, HIGH);
+      resultado_anterior=resultado;
   }
   Serial.println(resultado);
   delay(50);
